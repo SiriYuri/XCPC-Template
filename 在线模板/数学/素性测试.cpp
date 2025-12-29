c2b4c9b0f41f42b7037f4a8096711b1f82e35b54
Elysium <422428354@qq.com>
2025-12-30 02:31:20 +0800

    Create 素性测试.cpp

---
+++ 在线模板/数学/素性测试.cpp
@@ -0,0 +1,76 @@
+namespace Primes {
+  int mul(int a, int b, int m) {
+    int r = a * b - m * (int)(1.L / m * a * b);
+    return r - m * (r >= m) + m * (r < 0);
+  }
+  
+  int ModPow(int a, int b, int m) {
+    int res = 1 % m;
+    for (b; b; b >>= 1, a = mul(a, a, m)) {
+      if (b & 1) res = mul(res, a, m);
+    }
+    return res;
+  }
+
+  vector<int> B = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+
+  bool Miller_Rabin(int n) {
+    if (n <= 1) return 0;
+    for (int p : B) {
+      if (n == p) return 1;
+      if (n % p == 0) return 0;
+    }
+    int m = (n - 1) >> __builtin_ctz(n - 1);
+    for (int p : B) {
+      int t = m, a = ModPow(p, m, n);
+      while(t != n - 1 && a != 1 && a != n - 1) {
+        a = mul(a, a, n);
+        t *= 2;
+      }
+      if (a != n - 1 && t % 2 == 0) return 0;
+    }
+    return 1;
+  }
+  
+  int Pollard_Rho(int n) {
+    for (int p : B) {
+      if (n % p == 0) return p;
+    }
+
+    auto f = [&](int x) -> int {
+      x = mul(x, x, n) + 1;
+      return x >= n ? x - n : x;
+    };
+    
+    int x = 0, y = 0, tot = 0, p = 1, q, g;
+    for (int i = 0; (i & 255) || (g = gcd(p, n)) == 1; i++, x = f(x), y = f(f(y))) {
+      if (x == y) {
+        x = tot++;
+        y = f(x);
+      }
+      q = mul(p, abs(x - y), n);
+      if (q) p = q;
+    }
+    return g;
+  }
+
+  vector<int> fac(int n) {
+    if (n == 1) return {};
+    if (Miller_Rabin(n)) return {n};
+    int d = Pollard_Rho(n);
+    auto v1 = fac(d), v2 = fac(n / d);
+    auto i1 = v1.begin(), i2 = v2.begin();
+    vector<int> ans;
+    while(i1 != v1.end() || i2 != v2.end()) {
+      if (i1 == v1.end()) {
+        ans.emplace_back(*i2++);
+      } else if (i2 == v2.end()) {
+        ans.emplace_back(*i1++);
+      } else {
+        if (*i1 < *i2) ans.emplace_back(*i1++);
+        else ans.emplace_back(*i2++);
+      }
+    }
+    return ans;
+  }
+};
