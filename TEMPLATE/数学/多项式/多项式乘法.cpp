a033308630259d69c66000a2cd07c70bd63dcf96
Elysium <422428354@qq.com>
2026-01-05 02:07:19 +0800

    Create 多项式乘法.cpp

---
+++ TEMPLATE/数学/多项式/多项式乘法.cpp
@@ -0,0 +1,80 @@
+constexpr int mod = 998244353;
+
+using Poly = std::vector<int>;
+
+int power(int a, int b) {
+    int res = 1;
+    for (; b; b /= 2, a = 1LL * a * a % mod) {
+        if (b % 2) {
+            res = 1LL * res * a % mod;
+        }
+    }
+    return res;
+}
+
+std::vector<int> rev, roots {0, 1};
+
+void dft(std::vector<int> &a) {
+    int n = a.size();
+    if (int(rev.size()) != n) {
+        int k = __builtin_ctz(n) - 1;
+        rev.resize(n);
+        for (int i = 0; i < n; i++) {
+            rev[i] = rev[i >> 1] >> 1 | (i & 1) << k;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (rev[i] < i) {
+            std::swap(a[i], a[rev[i]]);
+        }
+    }
+    if (roots.size() < n) {
+        int k = __builtin_ctz(roots.size());
+        roots.resize(n);
+        while ((1 << k) < n) {
+            int e = power(31, 1 << (__builtin_ctz(mod - 1) - k - 1));
+            for (int i = 1 << (k - 1); i < (1 << k); i++) {
+                roots[2 * i] = roots[i];
+                roots[2 * i + 1] = 1LL * roots[i] * e % mod;
+            }
+            k++;
+        }
+    }
+    for (int k = 1; k < n; k *= 2) {
+        for (int i = 0; i < n; i += 2 * k) {
+            for (int j = 0; j < k; j++) {
+                int u = a[i + j];
+                int v = 1LL * a[i + j + k] * roots[k + j] % mod;
+                a[i + j] = (u + v) % mod;
+                a[i + j + k] = (u - v + mod) % mod;
+            }
+        }
+    }
+}
+
+void idft(std::vector<int> &a) {
+    int n = a.size();
+    std::reverse(a.begin() + 1, a.end());
+    dft(a);
+    int inv = power(n, mod - 2);
+    for (int i = 0; i < n; i++) {
+        a[i] = 1LL * a[i] * inv % mod;
+    }
+}
+
+std::vector<int> mul(std::vector<int> a, std::vector<int> b) {
+    int n = 1, tot = a.size() + b.size() - 1;
+    while (n < tot) {
+        n *= 2;
+    }
+    a.resize(n);
+    b.resize(n);
+    dft(a);
+    dft(b);
+    for (int i = 0; i < n; i++) {
+        a[i] = 1LL * a[i] * b[i] % mod;
+    }
+    idft(a);
+    a.resize(tot);
+    return a;
+}
